0f30db599c8f6bc98fae2db1560db6a3ba651a40
shahin-qoli <shahin-qoli@gmail.com>
2023-02-10 06:30:35 -0800

    stock component ok

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>The Stocks Component</h1>
+   <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+   </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue'
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: "BMX", price: 1200},
+                    {id: 2, name: "SAIPA", price: 200},
+                    {id: 3, name: "IRCO", price: 100},
+                    {id: 4, name: "BENZ", price: 1300}
+                ]
+
+            }
+        },
+        components:{
+            appStock: Stock,
+        }
+    }
+
+</script>
