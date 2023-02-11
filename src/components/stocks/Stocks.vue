b25378b9489a726e53807aee9dd0a9eb0e06cfab
shahin-qoli <shahin-qoli@gmail.com>
2023-02-11 01:21:05 -0800

    vuex for stocks handled

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -7,19 +7,13 @@
 <script>
     import Stock from './Stock.vue'
     export default {
-        data() {
-            return {
-                stocks: [
-                    {id: 1, name: "BMX", price: 1200},
-                    {id: 2, name: "SAIPA", price: 200},
-                    {id: 3, name: "IRCO", price: 100},
-                    {id: 4, name: "BENZ", price: 1300}
-                ]
-
-            }
-        },
         components:{
             appStock: Stock,
+        },
+        computed: {
+            stocks (){
+            return this.$store.stocks
+            }
         }
     }
 
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default 
+    [
+        {id: 1, name: "BMX", price: 1200},
+        {id: 2, name: "SAIPA", price: 200},
+        {id: 3, name: "IRCO", price: 100},
+        {id: 4, name: "BENZ", price: 1300}
+    ]
